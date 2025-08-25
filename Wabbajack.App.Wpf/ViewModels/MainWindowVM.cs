e6a57bb211b9cf5314003bc2a27dcdbbdcea7698
loregamer <162062830+loregamer@users.noreply.github.com>
2025-08-25 15:43:26 -0400

    Change window header

--- Wabbajack.App.Wpf/ViewModels/MainWindowVM.cs
+++ Wabbajack.App.Wpf/ViewModels/MainWindowVM.cs
@@ -190,7 +190,7 @@ public class MainWindowVM : ViewModel
             _logger.LogInformation("    Assembly Location: {AssemblyLocation}", assemblyLocation);
             _logger.LogInformation("    Process Location: {ProcessLocation}", processLocation);
 
-            WindowTitle = "Wabbajack UNLOCKED v2.4";
+            WindowTitle = "Wabbajack UNLOCKED v3.0";
 
             _logger.LogInformation("General information:");
             _logger.LogInformation("    Windows version: {Version}", Environment.OSVersion.VersionString);
